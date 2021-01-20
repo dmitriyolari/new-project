b760475a4da3d759904ee3d9f896bf95060825d8
Дмитрий <dumitrashikeu@gmail.com>
2021-01-20 10:49:36 +0200

    add shadow add checkScroll for fixed menu and comments for all .js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
-$(function () {
+$(function() {
 
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+    /*  Fixed Header*/
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function () {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        if( scrollOffset >= introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    /* Smooth scroll */
     $("[data-scroll]").on("click", function(event) {
         event.preventDefault();
 
@@ -15,6 +37,8 @@ $(function () {
         }, 500);
     });
 
+
+    /* Menu nav toggle */
     $("#nav_toggle").on("click", function(event) {
         event.preventDefault();
 
